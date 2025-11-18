4d830b48157faf6ef1707e73d2472b6d710ae18e
Kizzuato <kyyajuara@gmail.com>
2025-11-19 01:32:03 +0700

    second commit

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
+const e = require("express");
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "gh-itenas"
+  host: env.HOST,
+  user: env.USER,
+  password: env.PASSWORD,
+  database: env.DB_NAME,
 });
 
 module.exports = db;
